9389f25c3c70407e9f175e301679de9e65cadb2a
Purushothamreddy <purushothamreddydevana@gmail.com>
2022-08-16 19:51:18 +0530

    Javascript assignmentsoperators assignment

---
+++ assignments2.js
@@ -0,0 +1,7 @@
+var x=1;
+var x=x--;
+console.log(x);
+var x=x-1;
+console.log(x);
+var x=x-=1;
+console.log(x);
---
+++ assignmentsoperators.js
@@ -0,0 +1,7 @@
+var x=0;
+var x=x++;
+console.log(x);
+var x=x+1;
+console.log(x);
+var x=x+=1;
+console.log(x);
